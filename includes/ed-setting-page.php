2d24e6dc0ec08d77884c8e950c3672e94cf4725c
aramarket <aramarket.in@gmail.com>
2024-12-05 18:46:19 +0530

    Second change

--- includes/ed-setting-page.php
+++ includes/ed-setting-page.php
@@ -11,6 +11,7 @@ if (!class_exists('Easy_Dash_Setting')) {
 
         public function __construct() {
             add_action('admin_menu', array($this, 'register_menu_page'));
+			add_action( 'init', [$this, 'add_woocommerce_review_capabilities'] );
 
 			$this->$ew_testing_page = new Easy_WhatsApp_Testing_Page();
         }
@@ -24,6 +25,17 @@ if (!class_exists('Easy_Dash_Setting')) {
                 <H2>This is Home of EasyDash</H2>
             <?php
         }
+		
+		public function add_woocommerce_review_capabilities() {
+			// Get the role object, e.g., 'shop_manager'
+			$role = get_role('shop_manager'); // Replace 'shop_manager' with the desired role slug
+
+			// Add WooCommerce review management capabilities
+			$role->add_cap('moderate_comments'); // Required to moderate comments
+			$role->add_cap('edit_comment'); // Required to edit comments
+			$role->add_cap('read'); // Allows a user to read
+		}
+		
     }
 }
 
